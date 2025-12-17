6bc52d38100e7f320aad61d13c73f07a9cafc08b
dru1d-21 <eduard212005@gmail.com>
2025-12-18 01:24:29 +0300

    style: apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 // Main function that greets user
-int main() {
-std::string name;
-std::cout << "Enter your name: ";
-std::cin >> name;
-std::cout << "Hello world from " << name << std::endl;
-return 0; // Program finished successfully
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0; // Program finished successfully
 }
