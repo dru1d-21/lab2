49352d38a54ae9228177c145a23a26a1534cd7cc
dru1d-21 <147663385+dru1d-21@users.noreply.github.com>
2025-12-18 01:29:46 +0300

    Update hello_world.cpp to Russian

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-// Main function that greets user
+// Основная функция, которая приветствует пользователя!
 int main() {
 std::string name;
 std::cout << "Enter your name: ";
 std::cin >> name;
 std::cout << "Hello world from " << name << std::endl;
-return 0; // Program finished successfully
+return 0; // программа закончена успешно
 }
